5ae6985ad0284105c204c073cce8ce6b6368fe41
Juantr3jo <jmtm89ve@gmail.com>
2021-06-11 21:09:33 -0400

    todlist-

---
+++ src/Component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Component extends React.Component {
+    render() {
+        return <div>
+            <div id="trafictop"></div>
+            <div id="container">
+                <div className="red onClick(selected)=>"></div>
+                <div className="yellow "></div>
+                <div className="green "></div>
+            </div>;
+     </div>
+
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "poppers.js"
+import "./style.css"
+
+
+
+import Component from './Component.jsx';
+
+ReactDOM.render(
+  <Component/>,document.getElementById('root')
+);
